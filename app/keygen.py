5d098570f6d7788f9e5db91a01ded6dea802dc77
Eduardo Orsi <duduayr@gmail.com>
2024-03-31 14:24:09 -0300

    adding class to generate keys and ids

---
+++ app/keygen.py
@@ -0,0 +1,9 @@
+import secrets
+import string
+
+class Keygen:
+
+    @classmethod
+    def create_random_key(cls, length: int = 5) -> str:
+        chars = string.ascii_uppercase + string.digits
+        return "".join(secrets.choice(chars) for _ in range(length))
