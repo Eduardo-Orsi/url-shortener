a139bb9e96196b25216ae65cabca883f2d9255e9
Eduardo Orsi <duduayr@gmail.com>
2024-04-03 22:38:27 -0300

    feat: Implementing class to generate key for the URL in DB

--- app/keygen.py
+++ app/keygen.py
@@ -1,6 +1,8 @@
 import secrets
 import string
 
+from sqlalchemy.orm import Session
+
 
 class Keygen:
 
@@ -8,3 +10,11 @@ class Keygen:
     def create_random_key(cls, length: int = 5) -> str:
         chars = string.ascii_uppercase + string.digits
         return "".join(secrets.choice(chars) for _ in range(length))
+
+    @classmethod
+    def create_unique_random_key(cls, db: Session) -> str:
+        from .controller import URLController
+        key = Keygen.create_random_key()
+        while URLController.get_url_by_key(db, key):
+            key = Keygen.create_random_key()
+        return key
