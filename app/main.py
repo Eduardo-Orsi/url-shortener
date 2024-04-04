e71f00b5b6f1fd4afb5b80c7afe8763ee42e8149
Eduardo Orsi <duduayr@gmail.com>
2024-04-03 22:39:04 -0300

    feat: finalizing the main API core

--- app/main.py
+++ app/main.py
@@ -2,9 +2,8 @@ from sqlalchemy.orm import Session
 from fastapi.responses import RedirectResponse
 from fastapi import FastAPI, Request, HTTPException,Depends
 
-from .models import URL
-from .keygen import Keygen
 from .schemas import URLBase, URLInfo
+from .controller import URLController
 from .database import Base, engine, get_db
 
 app = FastAPI()
@@ -12,23 +11,28 @@ Base.metadata.create_all(bind=engine)
 
 @app.post("/create", response_model=URLInfo)
 async def create_short_url(url: URLBase, db: Session = Depends(get_db)):
-    key = Keygen.create_random_key()
-    secret_key = Keygen.create_random_key(8)
-    db_url = URL(
-        target_url=url.target_url,
-        key=key,
-        secret_key=secret_key
-    )
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
-    return db_url
+    db_url = URLController.create_url(db=db, url=url)
+    return URLController.get_admin_info(db_url)
 
 @app.get("/{url_key}")
 async def foward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
-    db_url = db.query(URL).filter(URL.key == url_key, URL.is_active).first()
+    db_url = URLController.get_url_by_key(db, url_key)
     if not db_url:
         raise HTTPException(404, f"URL '{request.url}' doesn't exist")
+    URLController.update_db_clicks(db, db_url)
     return RedirectResponse(db_url.target_url)
+
+@app.get("/admin/{secret_key}", name="Administration info", response_model=URLInfo)
+def get_url_info(secret_key: str, db: Session = Depends(get_db)):
+    url = URLController.get_url_by_secret_key(db, secret_key=secret_key)
+    if not url:
+        raise HTTPException(404, "screct key does not exist")
+    return URLController.get_admin_info(url)
+
+@app.delete("/admin/{secret_key}")
+def delete_url(secret_key: str, db: Session = Depends(get_db)):
+    db_url = URLController.deactivate_url_by_secret_key(db, secret_key)
+    if not db_url:
+        raise HTTPException(404, "The incorrect secret key")
+    message = f"Successfully deleted shortened URL for '{db_url.target_url}'"
+    return {"detail": message}
